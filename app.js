a8eb5b71413aa4d4ef976de0bc9e9e465fea27c3
hasan-bd <kamrulhasan570@gmail.com>
2021-06-08 12:08:00 +0600

    Show Post Title in colsole file using home js page use for loop

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(express.static("public"));
 let posts =[]
 
 app.get('/',function(req,res){
-  res.render('home',{startingContent: homeStartingContent})
-  console.log(posts);
+  res.render('home',{startingContent: homeStartingContent, post: posts})
+  // console.log(posts);
 })
 
 app.get('/about',function(req,res){
